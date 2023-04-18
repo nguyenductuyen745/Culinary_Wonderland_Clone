8a8a05f888f397a705e90f1d56e34a09a0d30b21
DESKTOP-H62OUVI\Duc Tuyen <nguyenductuyen745@gmail.com>
2023-04-18 23:22:09 +0700

    Base CW clone

---
+++ app/my-profile/[tab]/page.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export default function MyProfileContentPage({ params }: { params: { tab: string } }) {
+  const { tab } = params;
+  return (
+    <MyProfileContentWrap>
+
+    </MyProfileContentWrap>
+  )
+};
+
+const MyProfileContentWrap = styled.div`
+  
+`;
